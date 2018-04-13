e1a34d2d1cf4ce13e5e598d1e8dc13b2819469f3
nenadtod <nenadtod@live.com>
2018-04-13 13:58:32 +0200

    Added a project, with some code.

---
+++ RoseLibApp/RoseLib/Selectors/BaseSelector.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoseLibApp.RoseLib.Selectors
+{
+    public class BaseSelector
+    {
+        private SyntaxNode currentNode;
+        private List<SyntaxNode> currentNodesList;
+
+        public SyntaxNode CurrentNode
+        {
+            get { return currentNode; }
+            protected set {
+                currentNodesList = null;
+                currentNode = value;
+            }
+        }
+        public List<SyntaxNode> CurrentNodesList
+        {
+            get { return currentNodesList; }
+            set {
+                currentNode = null;
+                currentNodesList = value;
+            }
+        }
+
+
+        public BaseSelector()
+        {
+            currentNode = null;
+            currentNodesList = null;
+        }
+
+        public BaseSelector(SyntaxNode node)
+        {
+            currentNode = node;
+        }
+
+        public BaseSelector(List<SyntaxNode> nodes)
+        {
+            currentNodesList = nodes;
+        }
+    }
+}
